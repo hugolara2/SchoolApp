d8b02f9bb7648a00ee5b3738a033cb6d7dec346d
Hugo Lara <hugo.alex200197@outlook.com>
2023-08-02 21:38:27 -0600

    Adding new values for person

--- SchoolApp/Person/Person.cs
+++ SchoolApp/Person/Person.cs
@@ -2,22 +2,46 @@ namespace SchoolApp.Person;
 
 public abstract class Person
 {
-    private string _name;
+    private string _firstname;
+    private string _lastName;
     private int _age;
+    private string _curp;
+    private string _eid;
     private DateTime _birthday;
     private string _address;
     private string _city;
+    private string _phone;
+    private string _email;
+    private TypeOfCandidate _candidate;
 
-    public string Name
+    public string FirstName
     {
-        get => _name;
-        set => _name = value;
+        get => _firstname;
+        set => _firstname = value;
+    }
+
+    public string LastName
+    {
+        get => _lastName;
+        set => _lastName = value;
     }
 
     public int Age
     {
         get => _age;
         set => _age = value;
+    }
+
+    public string Curp
+    {
+        get => _curp;
+        set => _curp = value;
+    }
+
+    public string Eid
+    {
+        get => _eid;
+        set => _eid = value;
     }
 
     public DateTime Birthday
@@ -38,4 +62,26 @@ public abstract class Person
         set => _city = value;
     }
 
+    public string Phone
+    {
+        get => _phone;
+        set => _phone = value;
+    }
+
+    public string Email
+    {
+        get => _email;
+        set => _email = value;
+    }
+
+    public abstract void FirstRegistration(string firstName, string lastName, string email);
+    public abstract void ExtraInfoRegistration(string phone, TypeOfCandidate candidate);
+
+}
+
+public enum TypeOfCandidate
+{
+    Student = 1,
+    Teacher = 2,
+    Employee = 3
 }
